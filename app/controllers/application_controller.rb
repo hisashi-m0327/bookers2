81b611b854471104ecdd907dcd512c9dfa064bdf
HisashiMiyashita <225394446+hisashi-m0327@users.noreply.github.com>
2025-09-25 15:01:54 +0000

    [Add] 各リンク先の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
+  #ユーザー登録、ログイン認証などが使われる前に、下記configure_permitted_parameterメソッドが実行される
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def after_sign_in_path_for(resource) #ログイン後の遷移先を決めるメソッド
+    user_path(resource) #ログインしたユーザーの詳細ページ
+  end
 
   protected
 
+
+  #sign_upの際にnameのデータ操作を許可している
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
+   get "/homes/about" => "homes#about", as: "about"
+
   resources :books, only: [:new, :index, :show]
+  resources :users, only: [:index, :show]
 end
